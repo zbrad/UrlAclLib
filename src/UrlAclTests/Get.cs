cff3df04e0f6ad96fa310c3ce341025516c9a6f1
Brad Merrill <brad.merrill@skykick.com>
2016-02-23 11:02:33 -0800

    add urlacl source

---
+++ src/UrlAclTests/Get.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZBrad.UrlAclLib;
+
+namespace UrlAclTests
+{
+    [TestClass]
+    public class Get
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+            var h = new HttpApi();
+            var list = h.GetAcl();
+            Assert.AreNotEqual(0, list.Count);
+               
+        }
+    }
+}
